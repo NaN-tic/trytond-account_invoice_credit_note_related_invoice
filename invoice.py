c2f8d85eebd800e74b1b3e7aa6687e2373cf90cb
Raimon Esteve <raimon@nan-tic.com>
2018-03-13 13:14:50 +0100

    Upgrade from default branch From changeset-9622270

--- invoice.py
+++ invoice.py
@@ -2,7 +2,6 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval
 from trytond.transaction import Transaction
 
 __all__ = ['Invoice', 'CreditInvoice']
@@ -11,11 +10,9 @@ __all__ = ['Invoice', 'CreditInvoice']
 class Invoice:
     __metaclass__ = PoolMeta
     __name__ = 'account.invoice'
-    related_invoice = fields.Function(fields.Many2One('account.invoice',
-            'Related Invoice', states={
-                'invisible': Eval('invoice_type').in_(
-                    ['out_invoice', 'in_invoice']),
-                }, depends=['invoice_type']), 'get_related_invoice')
+    related_invoice = fields.Function(
+        fields.Many2One('account.invoice', 'Related Invoice'),
+        'get_related_invoice')
 
     @classmethod
     def get_related_invoice(cls, invoices, name):
@@ -25,7 +22,7 @@ class Invoice:
         invoice_ids = [i.id for i in invoices]
         result = {}.fromkeys(invoice_ids, None)
         for invoice in invoices:
-            if invoice.invoice_type in ('out_invoice', 'in_invoice'):
+            if '_invoice' in invoice.invoice_type_criteria():
                 continue
             for line in invoice.lines:
                 if isinstance(line.origin, InvoiceLine):
