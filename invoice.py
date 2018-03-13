05f2d4baae92894c772b2f5c7c40a50f3ada1a8b
Raimon Esteve <raimon@nan-tic.com>
2018-03-13 13:08:44 +0100

    Show error message when invoice state is not posted and paid task-033888

--- invoice.py
+++ invoice.py
@@ -43,7 +43,7 @@ class CreditInvoice:
         super(CreditInvoice, cls).__setup__()
         cls._error_messages.update({
                 'invoice_non_posted': ('You can not credit '
-                'invoice "%s" because it is not posted.'),
+                'invoice "%s" because it is not posted or paid.'),
             })
 
     def do_credit(self, action):
